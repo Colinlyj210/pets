b1864542768948fb497133106c0b16b932626641
colinlyj <lyj@lyjs-macbook-pro.local>
2015-08-07 22:24:39 +0800

    1.5

--- 宠物管家/HomeTableViewController.swift
+++ 宠物管家/HomeTableViewController.swift
@@ -14,11 +14,6 @@ class HomeTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.tableFooterView = UIView()
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,16 +33,70 @@ class HomeTableViewController: UITableViewController {
         return 5
     }
 
-   
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 400
+    }
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("homecell", forIndexPath: indexPath)
-
-        cell.textLabel?.text = "\(indexPath.row)"
-
+        switch indexPath.row {
+        case 0:
+            cell.contentView.addSubview(healthChart())
+        case 1:
+            cell.contentView.addSubview(eatChart())
+        case 2:
+            cell.textLabel?.text = "\(indexPath.row)"
+        case 3:
+            cell.textLabel?.text = "\(indexPath.row)"
+        case 4:
+            cell.textLabel?.text = "\(indexPath.row)"
+        default:
+            break
+        }
         return cell
     }
-    
-
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("\(indexPath.row)")
+    }
+    func healthChart()->UIView{
+        let items = [PNPieChartDataItem(value: 10 , color: UIColor.redColor()),PNPieChartDataItem(value: 20, color: UIColor.blueColor(), description: "abc"),PNPieChartDataItem(value: 40, color: UIColor.greenColor(), description: "123")]
+        let pie = PNPieChart(frame: CGRectMake(self.view.bounds.width/2 - 100, 100, 200, 200), items: items)
+        pie.descriptionTextColor = UIColor.whiteColor()
+        pie.descriptionTextFont = UIFont(name: "Avenir-Medium", size: 14)
+        pie.strokeChart()
+        return pie
+    }
+    func eatChart()->UIView{
+        let lineChart = PNLineChart(frame: CGRectMake(0, 0, self.view.frame.width , 200))
+        let xlab = ["sep1","sep2","sep3","sep4","sep5"];
+        lineChart.setXLabels(xlab, withWidth: 60)
+        let dataArr = [60.1,160.1,126.4,262.2,26.4]
+        let data = PNLineChartData()
+        data.color = UIColor.greenColor()
+        data.itemCount = UInt(xlab.count)
+        //data.getData = {()->()}
+        
+        data.getData = {(index: UInt) -> PNLineChartDataItem in
+            let yValue: CGFloat = CGFloat(dataArr[Int(index)])
+            return PNLineChartDataItem(y: yValue)
+        }
+        lineChart.chartData = [data]
+        lineChart.strokeChart()
+//        // Line Chart No.2
+//        NSArray * data02Array = @[@20.1, @180.1, @26.4, @202.2, @126.2];
+//        PNLineChartData *data02 = [PNLineChartData new];
+//        data02.color = PNTwitterColor;
+//        data02.itemCount = lineChart.xLabels.count;
+//        data02.getData = ^(NSUInteger index) {
+//            CGFloat yValue = [data02Array[index] floatValue];
+//            return [PNLineChartDataItem dataItemWithY:yValue];
+//        };
+//        
+//        lineChart.chartData = @[data01, data02];
+//        [lineChart strokeChart];
+        
+        
+        return lineChart
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
